829ef2e4186c01e01ff0fec14d8addd9e58a4b46
Dan Farrow <dan@zellement.com>
2023-05-11 22:23:17 +0100

    minor ui

--- components/atoms/SiteLogo.vue
+++ components/atoms/SiteLogo.vue
@@ -1,8 +1,11 @@
 <template>
-    <nuxt-link to="/" class="fixed top-0 left-0 p-4 m-0 font-serif text-4xl leading-[0.75] flex flex-col lg:flex-row lg:items-end group gap-2 lg:gap-0 2xl:p-16">
-        <span>Z</span>
+    <nuxt-link to="/" class="fixed top-0 left-0 p-4 m-0 font-serif text-4xl leading-[0.75] flex flex-col lg:flex-row lg:items-end group gap-2 lg:gap-0 2xl:p-16 2xl:flex-col 2xl:items-start 2xl:space-y-10">
+        <div class="relative self-start">
+            <span>Z</span>
+            <div class="absolute w-3/5 h-0.5 lg:h-1 -translate-x-1/2 bg-white left-1/2 top-1/2" />
+        </div>
 
-        <div class="flex flex-col gap-0.5 justify-end font-serif leading-none text-2xs lg:text-xs lg:pl-2 2xl:mb-px">
+        <div class="flex flex-col gap-0.5 justify-end font-serif leading-none text-2xs lg:text-xs lg:pl-2 2xl:mb-px 2xl:text-lg 2xl:pl-0 2xl:opacity-50">
             <h1 class="opacity-50 leading-[1]">Dan Farrow</h1>
             <h2 class="leading-[1]">Creative Developer</h2>
         </div>
