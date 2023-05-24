6a9f86ee5b7a0573b01bb1502255b8cce583be73
Dan Farrow <dan@zellement.com>
2023-05-24 17:38:33 +0100

    add active state

--- components/molecules/SiteNav.vue
+++ components/molecules/SiteNav.vue
@@ -1,15 +1,17 @@
 <template>
     <nav class="fixed bottom-0 left-0 z-20 flex flex-col p-4 font-serif text-lg list-none site-nav lg:text-xl xl:text-2xl xl:p-16 3xl:text-3xl ">
-        <li class="site-nav__list"><nuxt-link class="site-nav__link" to="/">Work</nuxt-link></li>
-        <li class="site-nav__list"><nuxt-link class="site-nav__link" to="/">About</nuxt-link></li>
-        <li class="site-nav__list"><nuxt-link class="site-nav__link" to="/">Contact</nuxt-link></li>
+        <li :class="isWorkPage" class="site-nav__list" ><nuxt-link class="site-nav__link" to="/">Work</nuxt-link></li>
+        <li class="site-nav__list"><nuxt-link class="site-nav__link" to="/about">About</nuxt-link></li>
+        <li class="site-nav__list"><nuxt-link class="site-nav__link" to="/contact">Contact</nuxt-link></li>
     </nav>
 </template>
 
 <script setup>
 const route = useRoute()
 
-console.log(route)
+const isWorkPage = computed(() => {
+    return route.path.includes('work') ? 'router-link-exact-active' : null
+})
 
 // const isWorkPage = computed(() => {
 //     return window.location.pathname.includes('work')
