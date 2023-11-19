e4bd7f3f084bb2b96bdac5f71f67348d82438acb
Dan Farrow <dan@zellement.com>
2023-11-19 14:15:22 +0000

    tweaks

--- components/molecules/SiteNav.vue
+++ components/molecules/SiteNav.vue
@@ -1,28 +1,73 @@
 <template>
     <nav class="fixed bottom-0 left-0 z-20 flex flex-col p-4 font-serif text-lg list-none site-nav lg:text-xl xl:text-2xl xl:p-16 3xl:text-3xl ">
-        <li :class="isWorkPage" class="site-nav__list" ><nuxt-link class="site-nav__link" to="/">Work</nuxt-link></li>
-        <li class="site-nav__list"><nuxt-link class="site-nav__link" to="/about">About</nuxt-link></li>
-        <li class="site-nav__list"><nuxt-link class="site-nav__link" to="/contact">Contact</nuxt-link></li>
-        <li class="flex flex-row items-center gap-4 mt-8">
-            <nuxt-link class="flex text-xs" to="/contact">
-                <Icon name="ri:linkedin-line" class="w-3 h-3 m-auto lg:w-6 lg:h-6" />
+        <li
+            :class="isWorkPage"
+            class="site-nav__list"
+        >
+            <nuxt-link
+                class="site-nav__link"
+                to="/"
+            >
+                Work
             </nuxt-link>
-            <nuxt-link class="flex text-xs" to="/contact">
-                <Icon name="ph:github-logo-light" class="w-3 h-3 m-auto lg:w-6 lg:h-6" />
+        </li>
+        <li class="site-nav__list">
+            <nuxt-link
+                class="site-nav__link"
+                to="/about"
+            >
+                About
+            </nuxt-link>
+        </li>
+        <li class="site-nav__list">
+            <nuxt-link
+                class="site-nav__link"
+                to="/contact"
+            >
+                Contact
+            </nuxt-link>
+        </li>
+        <li class="flex flex-row items-center gap-4 mt-8">
+            <nuxt-link
+                class="flex text-xs"
+                to="/contact"
+            >
+                <Icon
+                    name="ri:linkedin-line"
+                    class="w-3 h-3 m-auto lg:w-6 lg:h-6"
+                />
+            </nuxt-link>
+            <nuxt-link
+                class="flex text-xs"
+                to="/contact"
+            >
+                <Icon
+                    name="ph:github-logo-light"
+                    class="w-3 h-3 m-auto lg:w-6 lg:h-6"
+                />
             </nuxt-link>
             <button
                 :class="darkModeIconClasses"
                 class="flex"
-                @click="toggleColorMode('light')">
-                <Icon name="material-symbols:sunny-rounded" class="w-3 h-3 m-auto lg:w-6 lg:h-6" />
-            </button>
-            <button class="flex"
-                    :class="lightModeIconClasses"
-                    @click="toggleColorMode('dark')">
+                aria-label="Use light mode"
+                @click="toggleColorMode('light')"
+            >
                 <Icon
-                    name="material-symbols-light:nightlight" class="w-3 h-3 m-auto lg:w-6 lg:h-6" />
+                    name="material-symbols:sunny-rounded"
+                    class="w-3 h-3 m-auto lg:w-6 lg:h-6"
+                />
             </button>
-
+            <button
+                class="flex"
+                :class="lightModeIconClasses"
+                aria-label="Use dark mode"
+                @click="toggleColorMode('dark')"
+            >
+                <Icon
+                    name="material-symbols-light:nightlight"
+                    class="w-3 h-3 m-auto lg:w-6 lg:h-6"
+                />
+            </button>
         </li>
     </nav>
 </template>
