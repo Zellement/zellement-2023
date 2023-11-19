b31ca6b2900d19b8975a8e41528298a35bfebac3
Dan Farrow <dan@zellement.com>
2023-11-19 14:02:08 +0000

    finesse

--- components/atoms/SiteLogo.vue
+++ components/atoms/SiteLogo.vue
@@ -1,8 +1,8 @@
 <template>
-    <nuxt-link to="/" class="group fixed z-50 top-0 left-0 p-4 m-0 font-serif text-4xl leading-[0.75] flex flex-col lg:flex-row lg:items-end group gap-2 lg:gap-0 xl:p-16 xl:flex-col xl:items-start xl:space-y-10">
+    <nuxt-link to="/" class="group fixed z-50 top-0 left-0 p-4 m-0 text-black dark:text-white font-serif text-4xl leading-[0.75] flex flex-col lg:flex-row lg:items-end group gap-2 lg:gap-0 xl:p-16 xl:flex-col xl:items-start xl:space-y-10">
         <div class="relative self-start">
             <span>Z</span>
-            <div class="absolute w-3/5 h-0.5 lg:h-1 -translate-x-1/2 bg-white left-1/2 top-1/2 group-hover:w-3/4 transition-all duration-300" />
+            <div class="absolute w-3/5 h-0.5 lg:h-1 -translate-x-1/2 bg-current left-1/2 top-1/2 group-hover:w-3/4 transition-all duration-300" />
         </div>
 
         <div class="flex flex-col gap-0.5 justify-end font-serif leading-none text-2xs lg:text-xs lg:pl-2 xl:mb-px xl:text-lg xl:pl-0 xl:opacity-50">
--- components/molecules/SiteNav.vue
+++ components/molecules/SiteNav.vue
@@ -3,14 +3,52 @@
         <li :class="isWorkPage" class="site-nav__list" ><nuxt-link class="site-nav__link" to="/">Work</nuxt-link></li>
         <li class="site-nav__list"><nuxt-link class="site-nav__link" to="/about">About</nuxt-link></li>
         <li class="site-nav__list"><nuxt-link class="site-nav__link" to="/contact">Contact</nuxt-link></li>
+        <li class="flex flex-row items-center gap-4 mt-8">
+            <nuxt-link class="flex text-xs" to="/contact">
+                <Icon name="ri:linkedin-line" class="w-3 h-3 m-auto lg:w-6 lg:h-6" />
+            </nuxt-link>
+            <nuxt-link class="flex text-xs" to="/contact">
+                <Icon name="ph:github-logo-light" class="w-3 h-3 m-auto lg:w-6 lg:h-6" />
+            </nuxt-link>
+            <button
+                :class="darkModeIconClasses"
+                class="flex"
+                @click="toggleColorMode('light')">
+                <Icon name="material-symbols:sunny-rounded" class="w-3 h-3 m-auto lg:w-6 lg:h-6" />
+            </button>
+            <button class="flex"
+                    :class="lightModeIconClasses"
+                    @click="toggleColorMode('dark')">
+                <Icon
+                    name="material-symbols-light:nightlight" class="w-3 h-3 m-auto lg:w-6 lg:h-6" />
+            </button>
+
+        </li>
     </nav>
 </template>
 
 <script setup>
 const route = useRoute()
+const colorMode = useColorMode()
 
 const isWorkPage = computed(() => {
     return route.path.includes('work') ? 'router-link-exact-active' : null
+})
+
+const toggleColorMode = (value) => {
+    colorMode.preference = value
+}
+
+const isLightMode = computed(() => {
+    return colorMode.preference === 'light'
+})
+
+const lightModeIconClasses = computed(() => {
+    return isLightMode.value ? 'opacity-100' : 'opacity-50 pointer-events-none'
+})
+
+const darkModeIconClasses = computed(() => {
+    return isLightMode.value ? 'opacity-50 pointer-events-none' : 'opacity-100'
 })
 
 // const isWorkPage = computed(() => {
