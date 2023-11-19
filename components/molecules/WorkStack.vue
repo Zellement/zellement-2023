5c3d3ad407775ac6929de130101f48d88e1d8fc2
Dan Farrow <dan@zellement.com>
2023-11-19 15:10:16 +0000

    pad bot

--- components/molecules/WorkStack.vue
+++ components/molecules/WorkStack.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="relative flex w-1/2 h-screen max-h-screen ml-auto overflow-hidden md:w-3/5 lg:w-1/2" v-if="data">
-        <ul class="relative flex flex-col w-full h-full gap-4 py-4 pr-4 my-auto overflow-y-scroll xl:py-16 work-stack 2xl:pr-12">
+    <div
+        v-if="data"
+        class="relative flex w-1/2 h-screen max-h-screen ml-auto overflow-hidden md:w-3/5 lg:w-1/2"
+    >
+        <ul class="relative flex flex-col w-full h-full gap-4 py-4 pb-10 pr-4 my-auto overflow-y-scroll xl:py-16 work-stack 2xl:pr-12">
             <atoms-work-stack-item
                 v-for="item in data.allWorks"
                 :key="item.id"
