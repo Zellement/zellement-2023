4cef0bc4e93f2b3e0e993d06717ef6166eca4aea
Dan Farrow <dan@zellement.com>
2025-06-10 11:37:11 +0100

    remove strikethru

--- components/atoms/SiteLogo.vue
+++ components/atoms/SiteLogo.vue
@@ -3,11 +3,11 @@
         class="fixed transition-all space-y-4 duration-300 w-1/3 md:w-1/3 lg:w-1/2 z-20 top-0 left-0 pt-4 pl-4 m-0 pb-4 font-serif text-4xl leading-[0.75] flex flex-col pr-8 gap-2 lg:gap-0 xl:p-16 xl:flex-col xl:items-start xl:space-y-4"
     >
         <nuxt-link
-            class="relative self-start group"
+            class="relative self-start group hover:text-shiraz-500"
             to="/"
         >
             <span>Z</span>
-            <div class="absolute w-3/5 h-0.5 lg:h-1 -translate-x-1/2 bg-current left-1/2 top-1/2 group-hover:w-3/4 transition-width" />
+            <!-- <div class="absolute w-3/5 h-0.5 lg:h-1 -translate-x-1/2 bg-current left-1/2 top-1/2 group-hover:w-3/4 transition-width" /> -->
         </nuxt-link>
 
         <div
